7c805974e9225d481e03601f22061c2514adc3e7
Martina Quiroz Mantel <martu.92@hotmail.com>
2022-02-21 18:29:30 -0300

    Modifique el desafio

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let totalGanancia = 0;
 //Se contea desde el dia 1 hasta el dia 14 del mes de Enero
 for (let i = 1; i <= 14; i++) {
     //En cada repeticion, se pide la ganancia en decimales de cada dia de la quincena
-    totalGanancia = totalGanancia + parseFloat(prompt("Ingrese la ganancia del dia: " + i));
+    totalGanancia = totalGanancia + parseFloat(prompt("Ingrese la ganancia del dia: " + i + " de Enero"));
 }
 
-console.warn("En la 1ra quincena de Enero se gano un total de: ", totalGanancia);
+alert("En la 1ra quincena de Enero se ganó un total de: $" + totalGanancia);
